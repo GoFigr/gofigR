9389941dd7a2a207564e47118ca1d2ff10a35c54
Maciej Pacula <maciej@flagstaff.ai>
2025-03-05 14:39:20 -0500

    Fix gfconfig

--- R/gfconfig.R
+++ R/gfconfig.R
@@ -16,7 +16,7 @@ read_prompt <- function(prompt, validate=NULL, attempt=1, max_attempts=2) {
     tryCatch({
       val <- validate(res)
     }, error=function(err) {
-      warning(paste0(err, "\n"))
+      message(paste0(err, "\n"))
       return(read_prompt(prompt, validate=validate, attempt = attempt + 1))
     })
   } else {
@@ -51,8 +51,8 @@ login_with_username <- function(max_attempts) {
         stop("Unknown error occurred.")
       }
     }, error=function(err) {
-      warning(paste0(err, "\n"))
-      warning("Connection failed. Please verify your username & password and try again.\n\n")
+      message(paste0(err, "\n"))
+      message("Connection failed. Please verify your username & password and try again.\n\n")
       attempt <<- attempt + 1
     })
   }
