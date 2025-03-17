a1ef100f43b0ac35447fdaed511b90611d74d51e
Maciej Pacula <maciej@flagstaff.ai>
2025-03-17 14:13:59 -0400

    Up version and update docs

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 rm -f gofigR*tar.gz
+git rm -f man/*Rd
 rm -f man/*Rd
 R -e "roxygen2::roxygenize(clean=TRUE)"
+git add man/*Rd
 R CMD build .
 R CMD check --as-cran gofigR*tar.gz
 
